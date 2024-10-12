fc1e19930b02649bc347aee8d68aef34e1a2d8d9
Debopriyo <guddumajumder75@gmail.com>
2024-10-12 15:45:33 +0530

    Decode bencoded integers 02

--- cmd/mybittorrent/main.go
+++ cmd/mybittorrent/main.go
@@ -35,8 +35,7 @@ func decodeBencode(bencodedString string) (interface{}, error) {
 
 		return bencodedString[firstColonIndex+1 : firstColonIndex+1+length], nil
 	} else if bencodedString[0] == 'i' {
-		length := len(bencodedString)
-		return bencodedString[1 : length-1], nil
+		return strconv.Atoi(bencodedString[1 : len(bencodedString)-1])
 	} else {
 		return "", fmt.Errorf("Only strings are supported at the moment")
 	}
